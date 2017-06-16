f0585579d39f6b37f05676bdaf3509788e338d76
Joe Kramer <joekramer1125@gmail.com>
2017-06-16 16:06:54 -0700

    fix formatting on result page and add css to list of pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     $(".pizzaPage").hide();
     $(".resultPage").show();
     $("span#orderName").text(order.address.name);
-    $(".addressInfo").text(order.address.street + "</br>" + order.address.city + "," + order.address.state + order.address.zip);
+    $(".addressInfo").text(order.address.street + "<br />" + order.address.city + "," + order.address.state + " " + order.address.zip);
     order.pizzas.forEach(function(pizza) {
       $("#listPizzas").append("<li class='pizzaItem'>" + pizza.size + "</li>");
     });
